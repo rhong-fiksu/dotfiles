5885d2699f7a92bc1f58e128028d08723ba13fd0
Ronald Hong <ron@wistia.com>
2015-12-01 12:27:31 -0500

    Add sublime package installation script.

---
+++ sublime3/install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Backup current User settings
+cp -r ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User.backup &&
+
+# Remove current User settings
+rm -r ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User &&
+
+# Set symlink to saved settings
+ln -s `pwd`/sublime3/User ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
