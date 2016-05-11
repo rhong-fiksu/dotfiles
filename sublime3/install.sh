9d87207b2d8e752b41abe16b56e809807bbe2d04
Ronald Hong <ron@wistia.com>
2016-05-11 13:38:46 -0400

    Drop npm install stuff. Not very useful.

--- sublime3/install.sh
+++ sublime3/install.sh
@@ -8,7 +8,3 @@ rm -r ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User &&
 
 # Set symlink to saved settings
 ln -s `pwd`/sublime3/User ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
-
-# Install linty lint lint
-npm install -g --save-dev eslint-config-airbnb eslint-plugin-react eslint
-npm install -g jshint
