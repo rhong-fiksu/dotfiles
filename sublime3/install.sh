d738ef3f730d1d7c362c2779a11c619a7df8cbd4
Ronald Hong <ron@wistia.com>
2016-11-10 13:57:24 -0500

    Add install sublime and make required directories.

--- sublime3/install.sh
+++ sublime3/install.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+brew cask install sublime-text
+
+mkdir -p '~/Library/Application Support/Sublime Text 3/Packages/User'
+
 # Backup current User settings
 cp -r ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User.backup &&
 
