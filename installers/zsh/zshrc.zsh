f3ce9813cccfcf88088d8a1490a32c8415f59352
Ronald Hong <ron@wistia.com>
2016-11-17 12:01:55 -0500

    Add some Wistia setup stuff.

---
+++ installers/zsh/zshrc.zsh
@@ -0,0 +1,9 @@
+# Add this dir's bin to path, along with some other standard bin/sbin
+export PATH="$PATH:$ZSH_DIR/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
+
+source "$ZSH_DIR/antigen.zsh"
+source "$ZSH_DIR/editor.zsh"
+source "$ZSH_DIR/aliases.zsh"
+source "$ZSH_DIR/grc.zsh"
+source "$ZSH_DIR/rvm.zsh"
+source "$ZSH_DIR/wistia.zsh"
