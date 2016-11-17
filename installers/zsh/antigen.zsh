d7c545c5fabdbfaf7ebe63896441192325c8701d
Ronald Hong <ron@wistia.com>
2016-11-17 11:48:01 -0500

    Some bugfixes.

--- installers/zsh/antigen.zsh
+++ installers/zsh/antigen.zsh
@@ -6,9 +6,11 @@ ENABLE_CORRECTION="true"
 
 antigen use oh-my-zsh
 
-# antigen bundle command-not-found
+# For some reason, antigen doesn't work on first run unless there's a bundle
+# command listed. I don't know why.
+antigen bundle command-not-found
 # antigen bundle zsh-users/zsh-syntax-highlighting
 
-antigen theme https://gist.github.com/flushentitypacket/1669e39e1f0190bd7ae0c869b50f984c ron.zsh
+antigen theme https://gist.github.com/flushentitypacket/1669e39e1f0190bd7ae0c869b50f984c ron.zsh-theme
 
 antigen apply
