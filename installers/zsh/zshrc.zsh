fc48c05f969b7eb2074d5f27414840544f329459
Ronald Hong <ron@wistia.com>
2016-11-28 13:08:19 -0500

    Fix npm stuff with weird voodoo.

--- installers/zsh/zshrc.zsh
+++ installers/zsh/zshrc.zsh
@@ -1,5 +1,7 @@
 # Add this dir's bin to path, along with some other standard bin/sbin
 export PATH="$PATH:$ZSH_DIR/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin"
+# https://gist.github.com/DanHerbert/9520689
+export PATH="$HOME/.npm-packages/bin:$PATH"
 
 source "$ZSH_DIR/antigen.zsh"
 source "$ZSH_DIR/editor.zsh"
